55a63b64c423a054c87bdedd6ebdb6aeae8ca891
Bilgihan Kose <bilgihankose@gmail.com>
2020-08-22 16:26:31 +0300

    first commit

---
+++ Metabolism/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Metabolism
+//
+//  Created by Bilgihan Köse on 22.08.2020.
+//  Copyright © 2020 Bilgihan Kose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateButton(_ sender: Any) {
+        print(heightSlider.value)
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / (height * height)
+        print(bmi)
+        
+    }
+    
+    @IBAction func heightSliderValue(_ sender: UISlider) {
+        let heightSliderValue = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(heightSliderValue) M"
+    }
+    @IBAction func weightSliderValue(_ sender: UISlider) {
+        let weightSliderValue = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weightSliderValue) Kg"
+    }
+}
+
+
